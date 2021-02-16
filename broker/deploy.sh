85807a2b26c0e0332b66fe4f1adacb8d4cd6e49c
Tobias Kongsvik <tokongs98@gmail.com>
2021-02-16 12:55:13 +0100

    Change docker repo name

--- broker/deploy.sh
+++ broker/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull tokongs/ffi02:latest
+docker pull tokongs/ffi02-broker:latest
 docker stop broker
 docker rm broker
 docker run --name=broker --restart=always -p 8000:8000 -p 1883:1883 -d tokongs/ffi02:latest
