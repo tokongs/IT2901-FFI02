9cd049b26e79af36613ed0de4d5d4e9249d72bc3
Tobias Kongsvik <tokongs98@gmail.com>
2021-02-22 11:42:31 +0100

    Fix gradle setup and implement missing function in Mqtt3ClientConfig

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,6 @@ pluginManagement {
         id("com.github.johnrengelman.shadow") version "${extra["plugin.shadow.version"]}"
         id("biz.aQute.bnd.builder") version "${extra["plugin.bnd.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("com.jfrog.bintray") version "${extra["plugin.bintray.version"]}"
-        id("com.github.breadmoirai.github-release") version "${extra["plugin.github-release.version"]}"
         id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
         id("com.github.sgtsilvio.gradle.metadata") version "${extra["plugin.metadata.version"]}"
         id("com.github.sgtsilvio.gradle.javadoc-links") version "${extra["plugin.javadoc-links.version"]}"
@@ -17,8 +15,3 @@ pluginManagement {
 
 rootProject.name = "ffi02"
 include("client", "test-app")
-
-listOf("websocket", "proxy", "epoll", "reactor", "examples").forEach { module ->
-    include("client-$module")
-    project(":client-$module").projectDir = file(module)
-}
