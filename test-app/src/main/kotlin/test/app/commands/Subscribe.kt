a000843f468bdc64ada5d5d9a7f09287b7cbc738
Tobias Kongsvik <tokongs98@gmail.com>
2021-03-22 10:50:13 +0100

    Add test setup

---
+++ test-app/src/main/kotlin/test/app/commands/Subscribe.kt
@@ -0,0 +1,30 @@
+package test.app.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
+import com.hivemq.client.mqtt.datatypes.MqttQos
+import com.hivemq.client.mqtt.mqtt3.Mqtt3Client
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client
+import java.util.*
+
+class Subscribe : CliktCommand(printHelpOnEmptyArgs = true, help = "Subscribe to MQTT topic filter") {
+    private val brokerAddress by option("-b", "--broker-address", help = "MQTT Broker address").default("127.0.0.1")
+    private val filter by option("-f", "--topic-filter", help = "The topic to publish to").default("#")
+    private val qos by option("-q", "--qos", help = "Message qos").int().default(0)
+
+    override fun run() {
+        val client = Mqtt5Client.builder()
+            .identifier(UUID.randomUUID().toString())
+            .serverHost(brokerAddress)
+            .buildAsync()
+
+        client.connect()
+        client.subscribeWith()
+            .topicFilter(filter)
+            .qos(MqttQos.fromCode(qos) ?: MqttQos.AT_MOST_ONCE)
+            .callback {println("${it.topic}: ${String(it.payloadAsBytes)}")}
+            .send()
+    }
+}
